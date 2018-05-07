c3f03e1260d1348015ba05ab6dce5ea9140823e9
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2018-05-07 16:37:09 +0530

    Removed variable name from interface definition as type should be enough

--- fineractor.go
+++ fineractor.go
@@ -1,10 +1,10 @@
 package fineractor
 
 type Fineractor interface {
-	FundIncrement(fundId string, request FundIncrementRequest) (*FundIncrementResponse, error)
-	FundDecrement(fundId string, request FundDecrementRequest) (*FundDecrementResponse, error)
-	GetFundValue(fundId string, request FundValueRequest) (*FundValueResponse, error)
-	GetFunds(request FundsRequest) (*FundsResponse, error)
+	FundIncrement(string, FundIncrementRequest) (*FundIncrementResponse, error)
+	FundDecrement(string, FundDecrementRequest) (*FundDecrementResponse, error)
+	GetFundValue(string, FundValueRequest) (*FundValueResponse, error)
+	GetFunds(FundsRequest) (*FundsResponse, error)
 	GetFieldsMap() map[string]interface{}
 	MakeRequest(string, string, interface{}, interface{}) error
 }
