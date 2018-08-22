1ffe59bf76ecfccb801fa1e17967c905d9f90a77
Hung Nguyen <hung.nguyen.manh@trustingsocial.com>
2018-08-22 17:39:12 +0700

    auth api

---
+++ auth.go
@@ -0,0 +1,44 @@
+package fineract
+
+import (
+	"net/http"
+	"path"
+	"fmt"
+	"net/url"
+)
+
+type AuthResponse struct {
+	Username    string   `json:"username"`
+	UserId      int64    `json:"userId"`
+	Token       string   `json:"base64EncodedAuthenticationKey"`
+	OfficeId    int64    `json:"officeId"`
+	OfficeName  string   `json:"officeName"`
+	Permissions []string `json:"permissions"`
+}
+
+type AuthRequest struct {
+	Username string
+	Password string
+}
+
+func (client *Client) auth(r *AuthRequest) (*AuthResponse, error) {
+	if r.Username == "" || r.Password == "" {
+		return nil, fmt.Errorf("username and password can not be empty")
+	}
+
+	tempPath, err := url.Parse(authURLWithCredential(r))
+	if err != nil {
+		return nil, err
+	}
+
+	response := AuthResponse{}
+	err = client.MakeRequest(http.MethodPost, client.HostName.ResolveReference(tempPath).String(), nil, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+func authURLWithCredential(r *AuthRequest) string {
+	return path.Join(baseURL, fmt.Sprintf("authentication?username=%s&password=%s", r.Username, r.Password))
+}
---
+++ auth_test.go
@@ -0,0 +1,38 @@
+package fineract
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSuiteMockAuth(t *testing.T) {
+	if !testing.Short() {
+		t.Skip("Skipped mock tests in long mode")
+	}
+	client, err := makeClient(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	AuthSuite(t, client)
+}
+
+func AuthSuite(t *testing.T, client *Client) {
+	t.Run("Test authentication success", func(t *testing.T) {
+		authReq := &AuthRequest{
+			Username: "mifos",
+			Password: "mifos",
+		}
+
+		response, err := client.auth(authReq)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		require.Nil(t, err, "Error should be nil")
+		require.NotNil(t, response, "response should not be nil")
+		require.NotNil(t, response.Username, "username should not be nil")
+		require.NotNil(t, response.OfficeId, "OfficeId should not be nil")
+		require.NotNil(t, response.OfficeName, "OfficeName should not be nil")
+		require.NotNil(t, response.Permissions, "Permission should not be nil")
+		require.NotNil(t, response.Token, "Token should not be nil")
+	})
+}
